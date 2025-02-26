e2c7fa23e897f6d08de518a40227ba7c7c667c5b
Juliana V Maita <jujmaita24@gmail.com>
2025-02-25 22:23:05 -0300

    Exercício 3 - Classe e Instância

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,13 @@ function mensagemPersonalizada(nome: string): string {
     return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
 }
 console.log(mensagemPersonalizada("Ana"));
+
+// ex3
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) { }
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
