dc61884efe42717988430994e524318d31903922
Juliana V Maita <jujmaita24@gmail.com>
2025-02-25 22:20:49 -0300

    Exercicio 01:  Tipagem e Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// ex01
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
