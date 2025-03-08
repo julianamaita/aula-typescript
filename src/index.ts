9a6009f9871b8c95193b6bd76f086bca6434a8a2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-07 22:26:19 -0300

    Exercícios de TypeScript avançado

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-let mensagem: string = "Olá, TypeScript!";
-console.log(mensagem);
+// let mensagem: string = "Olá, TypeScript!";
+// console.log(mensagem);
 
-// ex01
-let valores: number[] = [10, 20, 30];
-let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
-console.log("Soma dos valores:", somaValores);
+// // ex01
+// let valores: number[] = [10, 20, 30];
+// let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+// console.log("Soma dos valores:", somaValores);
 
-// ex02
-function mensagemPersonalizada(nome: string): string {
-    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
-}
-console.log(mensagemPersonalizada("Ana"));
+// // ex02
+// function mensagemPersonalizada(nome: string): string {
+//     return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+// }
+// console.log(mensagemPersonalizada("Ana"));
 
-// ex3
-class Carro {
-    constructor(public marca: string, public modelo: string, public ano: number) { }
-    detalhes(): string {
-        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
-    }
-}
-const carro1 = new Carro("Toyota", "Corolla", 2022);
-console.log(carro1.detalhes());
+// // ex3
+// class Carro {
+//     constructor(public marca: string, public modelo: string, public ano: number) { }
+//     detalhes(): string {
+//         return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+//     }
+// }
+// const carro1 = new Carro("Toyota", "Corolla", 2022);
+// console.log(carro1.detalhes());
+
+
